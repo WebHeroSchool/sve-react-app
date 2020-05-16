71e39f927aa945ccdb2666e04d7855c9fe94ad64
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2020-05-16 14:19:33 +0300

    add directory with build-files

---
+++ build/precache-manifest.85a30226135b73c172166a6aae3efc31.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7bef1c21a8feb101b9cc6de8718046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b9c1247ed5e3f676acb",
+    "url": "/static/css/main.cc5be16e.chunk.css"
+  },
+  {
+    "revision": "ce60db8021eb7a22cfd8",
+    "url": "/static/js/2.7002a4a5.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/static/js/2.7002a4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9c1247ed5e3f676acb",
+    "url": "/static/js/main.66f2deba.chunk.js"
+  },
+  {
+    "revision": "525e17654fc1ad53d9c3",
+    "url": "/static/js/runtime-main.fb634b63.js"
+  },
+  {
+    "revision": "2cb86fca1681432c1f086222d1e3a7cc",
+    "url": "/static/media/button.2cb86fca.svg"
+  },
+  {
+    "revision": "ed09d0dd252fbe757532b220f79f271b",
+    "url": "/static/media/facebook.ed09d0dd.svg"
+  },
+  {
+    "revision": "f9f45406326ebf4c478ed8f52f3ae241",
+    "url": "/static/media/github.f9f45406.svg"
+  },
+  {
+    "revision": "9262307fd5b12aed6880082d0e5607c3",
+    "url": "/static/media/mail.9262307f.svg"
+  },
+  {
+    "revision": "7baca73c5a0a861d2643590c9e4de1ff",
+    "url": "/static/media/star.7baca73c.svg"
+  },
+  {
+    "revision": "8ea80a8c1cecb345a2f4a456d4d83dde",
+    "url": "/static/media/telegram.8ea80a8c.svg"
+  },
+  {
+    "revision": "c31353ce196ae2611a61f2351674c4ce",
+    "url": "/static/media/todo-nullItem.c31353ce.svg"
+  },
+  {
+    "revision": "0087379515fcd3812bb5c85e58cbfe96",
+    "url": "/static/media/union.00873795.svg"
+  },
+  {
+    "revision": "8cd6dc130de9643de6c4acf3c3b03a78",
+    "url": "/static/media/vk.8cd6dc13.svg"
+  }
+]);
